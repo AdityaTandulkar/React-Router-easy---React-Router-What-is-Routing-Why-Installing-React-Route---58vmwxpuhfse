646e1c46bc4eba536fad1b73110b3cf69ee07f4c
aditya <adityatandulkar20@gmail.com>
2023-03-28 20:34:51 +0530

    Initial commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Home = () => {
     return (
-        
+        <id id="home-page">Home Page</id>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const NavBar = () =>{
     return(
-
+        <nav>
+            <ul>
+                <li className='index-link'><NavLink to='/'>Index</NavLink></li>
+                <li className='home-link'><NavLink to='/home'>Home</NavLink></li>
+            </ul>
+        </nav>
     )
 }
